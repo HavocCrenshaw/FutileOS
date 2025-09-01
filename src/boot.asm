2857a3644b724e5c22700f47a55b15aab29bf8c1
HavocCrenshaw <havoccrenshaw@gmail.com>
2025-09-01 14:08:26 -0400

    yay characters

--- src/boot.asm
+++ src/boot.asm
@@ -2,7 +2,14 @@ org 0x7C00
 bits 16
 
 main:
-    hlt ; Nothing to be done
+    mov ah, 0x0A ; Write character
+    mov al, 0x48 ; Letter h
+    mov bh, 0x0 ; Page zero
+    mov cx, 0x4 ; Print 4 times
+
+    int 0x10 ; video interrupt
+
+    hlt ; Nothing left to be done
 
 .halt:
     jmp .halt ; Catch the processor if it tries to run away
