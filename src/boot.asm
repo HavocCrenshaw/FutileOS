b33cadc3a10fe565b5ba6613c910b9bfa40d21f4
HavocCrenshaw <havoccrenshaw@gmail.com>
2025-09-01 13:45:45 -0400

    actual dev begins

--- src/boot.asm
+++ src/boot.asm
@@ -1,1 +1,11 @@
-; Code here
+org 0x7C00
+bits 16
+
+main:
+    hlt ; Nothing to be done
+
+.halt:
+    jmp .halt ; Catch the processor if it tries to run away
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
