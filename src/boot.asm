241a21846a677861de7a7add3b4eab98ad5f5f75
HavocCrenshaw <havoccrenshaw@gmail.com>
2025-09-01 23:11:45 -0400

    added input/slight code improvements

--- src/boot.asm
+++ src/boot.asm
@@ -8,16 +8,27 @@ main:
 
 print:
     LODSB
-    CMP AL, 0 ; Make sure we're done
-    JZ halt ; If zero, go and halt
+    TEST AL, AL ; Make sure we're done
+    JZ input ; If zero, go and halt
     INT 0x10 ; Video interrupt
     JMP print
 
+input:
+    MOV AH, 0x0 ; Read key press
+    INT 0x16 ; Keyboard interrupt
+    CMP AL, 0x0D ; See if the key pressed is return
+    JE halt ; If so, let's get out
+
+    MOV AH, 0x0E
+    INT 0x10
+    JMP input ; Keep it looping
+
 halt:
-    HLT;
+    CLI ; Disable hardware interrupts
+    HLT
     JMP halt ; Catch the processor if it tries to run away
 
-hello: DB "Hello, World!", 0x0A, 0x0D, 0
+hello: DB "Type something, why don'tcha?", 0x0D, 0x0A, 0
 
 TIMES 510 - ($ - $$) DB 0
 DW 0xAA55
