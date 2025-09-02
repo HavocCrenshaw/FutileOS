bfce81a56b29bed24f70ab595e15243f2968db54
HavocCrenshaw <havoccrenshaw@gmail.com>
2025-09-02 13:30:18 -0400

    things are starting to look a bit more os like

--- src/boot.asm
+++ src/boot.asm
@@ -1,10 +1,15 @@
 ORG 0x7C00
 BITS 16
 
+clear:
+    MOV AH, 0x05 ; Select page
+    MOV AL, 0x1 ; Page 1
+    INT 0x10
+
 main:
-    MOV SI, hello
+    MOV SI, command
     MOV AH, 0x0E ; Teletype output, moving the cursor one to the right after printing a character.
-    MOV BH, 0x0 ; Page 0
+    MOV BH, 0x1 ; Page 0
 
 print:
     LODSB
@@ -16,19 +21,26 @@ print:
 input:
     MOV AH, 0x0 ; Read key press
     INT 0x16 ; Keyboard interrupt
-    CMP AL, 0x0D ; See if the key pressed is return
-    JE halt ; If so, let's get out
-
+    CMP AL, 0x0D ; Check if it's a carriage return
+    JE newline
     MOV AH, 0x0E
     INT 0x10
     JMP input ; Keep it looping
+
+newline:
+    MOV AH, 0x0E
+    MOV AL, 0x0D ; Return
+    INT 0x10
+    MOV AL, 0x0A ; Newline
+    INT 0x10
+    JMP input ; Keep it loopin
 
 halt:
     CLI ; Disable hardware interrupts
     HLT
     JMP halt ; Catch the processor if it tries to run away
 
-hello: DB "Type something, why don'tcha?", 0x0D, 0x0A, 0
+command: DB "(> ", 0
 
 TIMES 510 - ($ - $$) DB 0
 DW 0xAA55
